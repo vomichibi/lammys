db294ea8a80e09ce9f18e21b582637ed988ad37b
vomichibi <minhthien.vo1708@gmail.com>
2024-11-27 13:09:47 +0800

    feat: add getStripe function and Stripe client packages

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,4 +1,14 @@
 import Stripe from 'stripe'
+import { loadStripe } from '@stripe/stripe-js'
+
+let stripePromise: Promise<any> | null = null
+
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+  }
+  return stripePromise
+}
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
