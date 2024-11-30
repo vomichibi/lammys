a836be7cc4c9a5793cbd71541534b984f8946d93
vomichibi <minhthien.vo1708@gmail.com>
2024-12-01 01:09:14 +0800

    Global replace of firebase-admin import path to firebaseAdmin

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-import { auth } from '@/lib/firebase-admin'
+import { auth } from '@/lib/firebaseAdmin'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
